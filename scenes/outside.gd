a78b16d644ca1f3304d21961a1540706da9dec61
mondrehen <148411531+mondrehen@users.noreply.github.com>
2023-11-18 10:10:53 +0800

    Transition section, but there are 2 errors:can only access the transition once;export is not avaiable for var

--- scenes/outside.gd
+++ scenes/outside.gd
@@ -1,10 +1,10 @@
 extends LevelParent
-
+@export inside_level_scene:PackedScene = preload("res://scenes/inside.tscn")
 
 
 func _on_gate_player_entered_gate() -> void:
 	var tween = create_tween()
 	tween.tween_property($Player,"speed",0,2)
-	
 	print("player entered the gate area!")
+	get_tree().change_scene_to_packed(inside_level_scene)
 	pass # Replace with function body.
