74b24705a2b4c06e07b18aa812c4156e076990f3
mondrehen <148411531+mondrehen@users.noreply.github.com>
2024-04-19 16:01:03 +0800

    X5

--- scenes/box.gd
+++ scenes/box.gd
@@ -3,6 +3,9 @@ extends ContainerParent
 func hit():
 	if opened == false:
 		$LidSprite.hide()
-		var pos = $SpawnPositions.get_child(randi()%$SpawnPositions.get_child_count()).global_position
-		open.emit(pos,current_direction)
+		for i in range(5):
+			var pos = $SpawnPositions.get_child(randi()%$SpawnPositions.get_child_count()).global_position
+			open.emit(pos,current_direction)
+#			print(pos)
 		opened = true
+		
