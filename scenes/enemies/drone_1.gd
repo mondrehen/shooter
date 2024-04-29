8ab425adfd12a2df7b0af92d21c9244dc1833c7b
mondrehen <148411531+mondrehen@users.noreply.github.com>
2024-04-29 16:53:17 +0800

    Drone

--- scenes/enemies/drone_1.gd
+++ scenes/enemies/drone_1.gd
@@ -4,25 +4,58 @@ var active:bool = false
 var speed:int = 100
 var acceleration:int = 80
 var speedup_time:float = 0
-var explosion
+var explosion_range:int = 400
+var speeed_multiplier:int = 1
+var vulnerable:bool = true
+var health:int = 30
+var explosion_active:bool = false
+
 
 func _ready() -> void:
+	$explosion.hide()
 	$PointLight2D.energy = 0
 	pass
 	
 func _process(delta: float) -> void:
 	direction = (Globals.player_pos - global_position).normalized()
-	velocity = direction * (speed + acceleration * speedup_time)
+	velocity = direction * (speed + acceleration * speedup_time) * speeed_multiplier
 	if active:
 		look_at(Globals.player_pos)
 		speedup_time = speedup_time + delta
-		move_and_slide()
-		print(speedup_time)
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			$AnimationPlayer.play("blinking")
+			#explode() via AnimationPlayer
+			
 	if not active:
 		speedup_time = 0
+		
+	if explosion_active == true:
+		var targets = get_tree().get_nodes_in_group("Entity") + get_tree().get_nodes_in_group("container")
+		for target in targets:
+			var in_range = target.global_position.distance_to(global_position) < explosion_range
+			if "hit" in target and in_range:
+				target.hit()
+
+func stop_movement():
+	speeed_multiplier = 0
+	pass
+	
+func explode():
+	explosion_active = true
+	$AnimationPlayer.play("explosion")
+	stop_movement()
+
+			
 	
 func hit():
-
+	if vulnerable == true:
+		health -= 10
+		vulnerable = false
+		$VulnerableTimer.start()
+		$Drone.material.set_shader_parameter("progress",1)
+	if health <= 0:
+		explode()
 	pass
 
 
@@ -39,13 +72,16 @@ func _on_active_area_body_exited(body):
 
 
 
-func _on_readyto_boom_body_entered(body):
-	$AnimationPlayer.play("blinking")
-	$PointLight2D.energy = 0
-	active = false
-	await get_tree().create_timer(1.2).timeout
-	$AnimationPlayer.play("explosion")
-	print("ready to explode")
-	await $AnimationPlayer.animation_finished
-	queue_free()
+#func _on_readyto_boom_body_entered(body):
+#	$AnimationPlayer.play("blinking")
+#	stopandboom = true
+#	$PointLight2D.energy = 0
+#	await get_tree().create_timer(1.2).timeout
+#	$AnimationPlayer.play("explosion")
+#	pass # Replace with function body.
+
+
+func _on_vulnerable_timer_timeout():
+	vulnerable = true
+	$Drone.material.set_shader_parameter("progress",0)
 	pass # Replace with function body.
